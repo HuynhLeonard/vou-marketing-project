cd9756787c0a678d75f1349c00562bd9def82b28
LeonardHuynh <thienhuuhuynh.dev@gmail.com>
2024-09-07 14:07:39 +0700

    upload APIGateway Module

---
+++ backend/src/api-gateway/src/main/java/api_gateway/WebClientConfig.java
@@ -0,0 +1,13 @@
+package api_gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
