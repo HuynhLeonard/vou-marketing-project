7b4331c7682e6168b63dc6846669a42fb27dc4eb
LeonardHuynh <thienhuuhuynh.dev@gmail.com>
2024-09-07 13:50:07 +0700

    Upload Cloudinary Service

--- backend/src/user-service/src/main/java/com/voufinal/userservice/controller/PlayerController.java
+++ backend/src/user-service/src/main/java/com/voufinal/userservice/controller/PlayerController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/players")
+@RequestMapping(value = "/api/v1/players")
 @CrossOrigin
 public class PlayerController {
     private final UserService userService;
