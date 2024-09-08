b2a4fdde2f9d03b0b305f197886bdabbc71247af
Lucas-VTL <votanloc1106@gmail.com>
2024-09-09 02:19:43 +0700

    Add web app

---
+++ frontend/web-app/src/redux/store.jsx
@@ -0,0 +1,31 @@
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import authReducer from "./auth";
+import { configureStore } from "@reduxjs/toolkit";
+import eventReducer from "./event";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["auth"],
+    blacklist: [],
+};
+
+const reducer = combineReducers({
+    auth: authReducer,
+    event: eventReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export const persistor = persistStore(store);
+export default store;
