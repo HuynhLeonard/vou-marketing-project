2b459e834c459c06834161093835b514b51cc985
Lucas-VTL <votanloc1106@gmail.com>
2024-09-10 16:10:46 +0700

    Add edit admin profile page

---
+++ frontend/web-app/src/pages/EditAdminPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../Components/Sidebar";
+import EditAdminForm from "../Components/EditAdminForm";
+
+function EditAdminPage() {
+    return (
+        <div>
+            <Sidebar />
+            <EditAdminForm />
+        </div>
+    );
+}
+
+export default EditAdminPage;
