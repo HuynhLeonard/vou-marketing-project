4d9a239865996e58780197237c0ccf220b3e7094
Lucas-VTL <votanloc1106@gmail.com>
2024-09-11 16:37:40 +0700

    Commit again

--- frontend/web-app/src/Components/Homepage.jsx
+++ frontend/web-app/src/Components/Homepage.jsx
@@ -10,6 +10,9 @@ import api from "../service/api";
 import axios from "axios";
 
 import eventDashboard from "../utils/images/eventDashboard.png";
+import userDashboard from "../utils/images/userDashboard.png";
+import brandDashboard from "../utils/images/brandDashboard.png";
+import voucherDashboard from "../utils/images/voucherDashboard.png";
 
 import { Link } from "react-router-dom";
 
@@ -22,89 +25,56 @@ function Homepage() {
                         <div class="font-bold sm:text-lg xl:text-xl 2xl:text-2xl text-info mb-5">
                             THÔNG SỐ TỔNG QUÁT
                         </div>
-                        <div class="grid grid-cols-2 w-full h-[500px] gap-4 ">
-                            <div class="card bg-base-100 flex">
+                        <div class="grid grid-cols-2 w-full h-[500px] gap-4">
+                            <div class="bg-base-100 flex rounded-2xl shadow-2xl">
                                 <img
-                                    class="object-cover w-1/3"
+                                    class="object-cover w-1/3 bg-blue-600"
+                                    src={userDashboard}
+                                    alt="User Dashboard"
+                                />
+                                <div class="card-body md:pl-0">
+                                    <div class="stat">
+                                        <div class="stat-title font-bold">Tổng số người dùng</div>
+                                        <div class="stat-value text-blue-600 text-8xl">150</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bg-base-100 flex rounded-2xl shadow-2xl">
+                                <img
+                                    class="object-cover w-1/3 bg-red-600"
+                                    src={brandDashboard}
+                                    alt="Brand Dashboard"
+                                />
+                                <div class="card-body md:pl-0">
+                                    <div class="stat">
+                                        <div class="stat-title font-bold">Tổng số nhãn hàng</div>
+                                        <div class="stat-value text-red-600 text-8xl">10</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bg-base-100 flex rounded-2xl shadow-2xl">
+                                <img
+                                    class="object-cover w-1/3 bg-green-600"
                                     src={eventDashboard}
                                     alt="Event Dashboard"
                                 />
-                                <div class="card-body">
+                                <div class="card-body md:pl-0">
                                     <div class="stat">
-                                        <div class="stat-title font-bold">Tổng số người dùng</div>
-                                        <div class="stat-value text-red-500">150</div>
+                                        <div class="stat-title font-bold">Tổng số sự kiện</div>
+                                        <div class="stat-value text-green-600 text-8xl">18</div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="card bg-base-100">
-                                <div class="card-body">
+                            <div class="bg-base-100 flex rounded-2xl shadow-2xl">
+                                <img
+                                    class="object-cover w-1/3 bg-yellow-600"
+                                    src={voucherDashboard}
+                                    alt="Voucher Dashboard"
+                                />
+                                <div class="card-body md:pl-0">
                                     <div class="stat">
-                                        <div class="stat-figure text-primary">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                class="inline-block h-8 w-8 stroke-current"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    stroke-width="2"
-                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                                                ></path>
-                                            </svg>
-                                        </div>
-                                        <div class="stat-title">Total Likes</div>
-                                        <div class="stat-value text-primary">25.6K</div>
-                                        <div class="stat-desc">21% more than last month</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card bg-base-100">
-                                <div class="card-body">
-                                    <div class="stat">
-                                        <div class="stat-figure text-primary">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                class="inline-block h-8 w-8 stroke-current"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    stroke-width="2"
-                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                                                ></path>
-                                            </svg>
-                                        </div>
-                                        <div class="stat-title">Total Likes</div>
-                                        <div class="stat-value text-primary">25.6K</div>
-                                        <div class="stat-desc">21% more than last month</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card bg-base-100">
-                                <div class="card-body">
-                                    <div class="stat">
-                                        <div class="stat-figure text-primary">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                class="inline-block h-8 w-8 stroke-current"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    stroke-width="2"
-                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                                                ></path>
-                                            </svg>
-                                        </div>
-                                        <div class="stat-title">Total Likes</div>
-                                        <div class="stat-value text-primary">25.6K</div>
-                                        <div class="stat-desc">21% more than last month</div>
+                                        <div class="stat-title font-bold">Tổng số voucher</div>
+                                        <div class="stat-value text-yellow-600 text-8xl">80</div>
                                     </div>
                                 </div>
                             </div>
@@ -119,7 +89,7 @@ function Homepage() {
                             class="w-full h-full z-10"
                             leftControl=" "
                             rightControl=" "
-                            indicators="false"
+                            indicators={false}
                         >
                             <Link to="/admin/userManagement">
                                 <img
