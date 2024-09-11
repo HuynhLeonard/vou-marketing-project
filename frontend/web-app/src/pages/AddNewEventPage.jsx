b2def499877ad3d93096b12afd37bac9e9bef45f
Lucas-VTL <votanloc1106@gmail.com>
2024-09-12 02:48:20 +0700

    Add new event route

---
+++ frontend/web-app/src/pages/AddNewEventPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import BrandSideBar from "../Components/BrandSideBar";
+import AddNewEventForm from "../Components/AddNewEventForm";
+
+function AddNewEventPage() {
+    const categoryData = [
+        { name: "TRANG CHỦ", navigation: "/brand", title: "homepage-title" },
+        {
+            name: "QUẢN LÝ SỰ KIỆN",
+            navigation: "/brand/eventManagement",
+            title: "eventManagement-title",
+        },
+    ];
+
+    return (
+        <div>
+            <BrandSideBar categoryData={categoryData} />
+            <AddNewEventForm />
+        </div>
+    );
+}
+
+export default AddNewEventPage;
